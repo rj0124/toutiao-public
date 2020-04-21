e4642ab15207d201fe547ac4b16a33e4c8bcb847
xiaomeihao <317991755@qq.com>
2020-04-21 09:12:46 +0800

    第二次提交

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,14 @@
+// 导入axios
 import axios from 'axios'
 
+// 创建axios实例
+// 我们通过这个实例去发送请求,把需要的配置配置给这个实例
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/'
+  // 请求的基本路径
+  baseURL: 'http://ttapi.research.itcast.cn'
 })
 
+// 导出request模块
 export default request
+
+// 谁使用,谁加载
