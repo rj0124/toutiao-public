1c1df7e41cfdc0d1692c85af57cfcd82e5321ffc
xiaomeihao <317991755@qq.com>
2020-04-27 22:32:28 +0800

    完成编辑

---
+++ src/views/publish/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="publish-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ $route.query.id ? '修改文章' : '发布文章' }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form ref="form" :model="article" label-width="40px">
+        <el-form-item label="标题">
+          <el-input v-model="article.title"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="article.content"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-radio-group v-model="article.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="article.channel_id" placeholder="请选择频道">
+            <el-option v-for="(channel, index) in channels" :key="index" :label="channel.name" :value="channel.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onPublish(false)">发布</el-button>
+          <el-button @click="onPublish(true)">存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+<script>
+import { getArticleChannels, addArticle, updateArticle, getArticle } from '@/api/article'
+
+export default {
+  name: 'publishIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      channels: [], // 文章频道列表
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 封面类型
+          images: [] // 封面图片的地址
+        },
+        channel_id: null
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    this.loadChannels()
+
+    // 由于发布与修改用的同一个组件,所以这里要判断
+    // 如果路由路径参数里面有id,则请求展示文章内容
+    if (this.$route.query.id) {
+      this.loadArticle()
+    }
+  },
+  mounted () {},
+  methods: {
+    loadChannels () {
+      getArticleChannels().then(({ data }) => {
+        // console.log(res)
+        this.channels = data.data.channels
+      })
+    },
+    onPublish (draft = false) {
+      // 找到数据接口
+      // 封装请求方法
+      // 请求提交表单
+      // 如果是编辑文章,则执行修改操作
+      const articleId = this.$route.query.id
+      if (this.$route.query.id) {
+        updateArticle(articleId, this.article, draft).then(res => {
+          this.$message({
+            message: `${draft ? '存入草稿' : '发布'}成功`,
+            type: 'success'
+          })
+          // 跳转到内容管理页面
+          this.$router.push('/article')
+        })
+      } else {
+        addArticle(this.article, draft).then(res => {
+          console.log(res)
+          this.$message({
+            message: `${draft ? '存入草稿' : '发布'}成功`,
+            type: 'success'
+          })
+          // 跳转到内容管理页面
+          this.$router.push('/article')
+        })
+      }
+      // 处理响应结果
+    },
+
+    // 编辑文章
+    loadArticle () {
+      console.log('loadArticle')
+      // 找到数据接口
+      // 封装请求方法
+      // 请求获取数据
+      getArticle(this.$route.query.id).then(res => {
+        // 模块绑定展示
+        this.article = res.data.data
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
