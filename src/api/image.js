11bb3f21466d45c94bcbcbb8cc3638eb773ce90c
xiaomeihao <317991755@qq.com>
2020-04-28 22:32:51 +0800

    素材管理图片上传成功

--- src/api/image.js
+++ src/api/image.js
@@ -1,10 +1,20 @@
 import request from '@/utils/request'
 
+// 上传图片素材
 export const uploadImage = data => {
   return request({
     method: 'POST',
     url: '/mp/v1_0/user/images',
     // 一般文件上传的接口都要求把请求头中的 Content-Type 设置为 multipart/from-data,但是我们使用axios上传文件的话不需要手动设置,只需要给data一个FormData对象即可
     data
+  })
+}
+
+// 获取图片素材
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
   })
 }
