e5c95c51cf3e2440c53685037555a885d5cc2041
xiaomeihao <317991755@qq.com>
2020-04-29 22:31:04 +0800

    完成图片预览

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Layout from '@/views/layout/'
 import Article from '@/views/article/'
 import Publish from '@/views/publish/'
 import Image from '@/views/image/'
+import Comment from '@/views/comment/'
+import Settings from '@/views/settings/'
 
 Vue.use(VueRouter)
 
@@ -43,6 +45,16 @@ const routes = [
         path: '/image',
         name: 'image',
         component: Image
+      },
+      {
+        path: '/comment',
+        name: 'comment',
+        component: Comment
+      },
+      {
+        path: '/settings',
+        name: 'settings',
+        component: Settings
       }
     ]
   }
